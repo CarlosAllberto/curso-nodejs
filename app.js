63397e71822b63482eb4f62d6a70f90b19f1ab75
CarlosAllberto <dasilvacarlosalberto344@gmail.com>
2023-10-20 13:58:35 -0300

    adicionando mustache

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-const express = require("express")
-const router = require("./routes")
+const express = require('express')
+const router = require('./routes')
+const mustache = require('mustache-express')
 
 const app = express()
 app.use('/', router)
+
+app.use(express.json())
+
+app.engine('mst', mustache())
+app.set('view engine', 'mst')
+app.set('views', __dirname + '/views')
 
 module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,30 @@
 const express = require('express')
-
 const router = express.Router()
-router.get('/', res => res.send('página inicial'))
-router.get('/sobre', res => res.send('página sobre'))
+
+router.get('/', (req, res) => {
+    let obj = { 
+        nome: req.query.nome,
+        idade: req.query.idade,
+        mostrar: true,
+        ingredientes: [
+            { nome: 'Arroz', qt: '20g' },
+            { nome: 'Macarrão', qt: '100g' }
+        ]
+    }
+    res.render('home', obj)
+})
+
+router.get('/user', (req, res) => {
+    let nome = req.query.nome
+    res.send(`Olá, ${nome}`)
+    res.json(req.query)
+})
+
+router.get('/sobre', (req, res) => res.send('página sobre'))
+
+router.get('/posts/:id', (req, res) => {
+    let id = req.params.id
+    res.send(`post: ${id}`)
+})
 
 module.exports = router
