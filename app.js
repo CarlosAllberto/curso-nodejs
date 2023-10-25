a7072965702e3a4cc5ab49cb36a26dc8f0b60c36
CarlosAllberto <dasilvacarlosalberto344@gmail.com>
2023-10-25 14:03:41 -0300

    mudando arquitetura de pastas

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const router = require('./routes')
+const router = require('./src/routes')
 const mustache = require('mustache-express')
 const helpers = require('./helpers')
 
